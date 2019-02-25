906dc02064ebfa4615f0dabfc262669965f20775
Vanessa Sanjinez <vanessa.sanjinez@truextend.com>
2019-02-25 16:30:32 -0400

    Add routes on app js file for different components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Form from "./Form.js";
+import Validator from "./Validator.js";
+import Test from "./Test.js";
+import AgeValidator from "./AgeValidator.js";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/form" exact={true} component={Form} />
+          <Route path="/validator" exact={true} component={Validator} />
+          <Route path="/test" exact={true} component={Test} />
+          <Route path="/age" extact={true} component={AgeValidator} />
+        </Switch>
+      </Router>
     );
   }
 }
